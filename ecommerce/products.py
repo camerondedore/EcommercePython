1e73bac7043072790cc45e683e24602469f49145
CallieAnn <dedorec@my.lanecc.edu>
2019-01-31 20:05:11 -0800

    added index page, base html, setting up for other pages

---
+++ ecommerce/products.py
@@ -0,0 +1,15 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+bp = Blueprint('products', __name__)
+
+
+@bp.route('/')
+def index():
+
+    return render_template('product/home.html')
